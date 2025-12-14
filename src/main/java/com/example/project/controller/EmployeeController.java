fbd345c11753a103b6001aa8057a57e2ce4b6f8f
Martin2446 <martingrozev87@gmail.com>
2025-12-14 16:34:11 +0200

    created saveEmployee, getAllEmployees and deleteEmployee functions

--- src/main/java/com/example/project/controller/EmployeeController.java
+++ src/main/java/com/example/project/controller/EmployeeController.java
@@ -1,4 +1,45 @@
 package com.example.project.controller;
 
+import com.example.project.dto.EmployeeRequestDTO;
+import com.example.project.dto.EmployeeResponseDTO;
+import com.example.project.mapper.EmployeeMapper;
+import com.example.project.model.Employee;
+import com.example.project.service.EmployeeService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+@RequiredArgsConstructor
+@Validated
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+    private final EmployeeMapper employeeMapper;
+
+    @PostMapping
+    public ResponseEntity<EmployeeResponseDTO> saveEmployee(@Valid @RequestBody EmployeeRequestDTO employeeRequestDTO) {
+        Employee savedEmployee = employeeService.saveEmployee(employeeRequestDTO);
+        return new ResponseEntity<>(employeeMapper.toEmployeeResponseDTO(savedEmployee), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EmployeeResponseDTO>> getAllEmployees() {
+        return new ResponseEntity<>(employeeMapper.toEmployeeResponseDTOList(employeeService.getAllEmployees()), HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteEmployee(@PathVariable @Positive Long id)
+    {
+        employeeService.deleteEmployee(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/example/project/service/EmployeeService.java
+++ src/main/java/com/example/project/service/EmployeeService.java
@@ -1,4 +1,45 @@
 package com.example.project.service;
 
+import com.example.project.controller.EmployeeController;
+import com.example.project.dto.EmployeeRequestDTO;
+import com.example.project.mapper.EmployeeMapper;
+import com.example.project.model.Employee;
+import com.example.project.repository.EmployeeRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class EmployeeService {
+
+    private final EmployeeController employeeController;
+    private final EmployeeMapper employeeMapper;
+    private final EmployeeRepository employeeRepository;
+
+    @Transactional
+    public Employee saveEmployee(EmployeeRequestDTO employeeRequestDTO)
+    {
+        Employee employee = employeeMapper.toEmployee(employeeRequestDTO);
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees()
+    {
+        return employeeRepository.findAll();
+    }
+
+    @Transactional
+    public void deleteEmployee(Long id)
+    {
+        if(employeeRepository.existsById(id))
+            employeeRepository.deleteById(id);
+        else
+            throw new EntityNotFoundException("Employee by this id" + id + "does not  exist!");
+    }
 }
