6d39a5ea60a36cd0755121ef6053781bd76c8de3
Martin2446 <martingrozev87@gmail.com>
2025-12-15 23:18:46 +0200

    cleaned up the file

--- src/main/java/com/example/project/controller/ProductController.java
+++ src/main/java/com/example/project/controller/ProductController.java
@@ -15,8 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.swing.text.html.parser.Entity;
-
 @RestController
 @RequestMapping("/products")
 @RequiredArgsConstructor
