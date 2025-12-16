0722acda549310012f5f82a878bc41f5416530bc
Martin2446 <martingrozev87@gmail.com>
2025-12-16 15:18:33 +0200

    cleaned up the code

--- src/main/java/com/example/project/mapper/EmployeeMapper.java
+++ src/main/java/com/example/project/mapper/EmployeeMapper.java
@@ -10,29 +10,10 @@ import java.util.List;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface EmployeeMapper {
 
-    @Mapping(target = "departmentId", source = "department.id")
-    @Mapping(target = "departmentName", source = "department.name")
     EmployeeResponseDTO toEmployeeResponseDTO(Employee employee);
 
     List<EmployeeResponseDTO> toEmployeeResponseDTOList(List<Employee> employees);
 
-    EmployeeSummaryResponseDTO toEmployeeSummaryResponseDTO(Employee employee);
-
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "department", ignore = true)
     Employee toEmployee(EmployeeRequestDTO employeeRequestDTO);
-
-    List<EmployeeNameResponseDTO> toEmployeeNameResponseDTOList(List<EmployeeNameProjection> employeeNameProjections);
-
-    List<EmployeeNameResponseDTO> toEmployeeNameResponseDTOListFromProjectionDTOList(List<EmployeeNameProjectionDTO> employeeNameProjectionDTOs);
-
-    @Mapping(target = "department", ignore = true)
-    void updateEmployeeFromRequestDTO(@MappingTarget Employee entity, EmployeeRequestDTO dto);
-
-    @AfterMapping
-    default void trimEmployeeFields(@MappingTarget Employee entity, EmployeeRequestDTO dto) {
-        entity.setFirstName(dto.firstName().trim());
-        entity.setLastName(dto.lastName().trim());
-        if (entity.getPhoneNumber() != null) entity.setPhoneNumber(dto.phoneNumber().trim());
-    }
 }
